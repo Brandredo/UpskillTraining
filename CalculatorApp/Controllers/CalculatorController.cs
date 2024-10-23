ece540c382cebd1e0aee4258b3138e4a333d2b71
Brandon <figueredo407@gmail.com>
2024-10-23 10:34:27 -0400

    Add project files.

---
+++ CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        [HttpPost("Add/{num1}/{num2}")]
+        public ActionResult<int> Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpPost("Subtract/{num1}/{num2}")]
+        public ActionResult<int> Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        [HttpPost("Multiply/{num1}/{num2}")]
+        public ActionResult<int> Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        [HttpPost("Divide/{num1}/{num2}")]
+        public ActionResult<decimal> Divide(decimal num1, decimal num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ CounterApp/Controllers/CounterController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CounterApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+
+
+
+
+    }
+}
